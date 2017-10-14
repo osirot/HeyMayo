876b356df6f54e5fc0f638da2839020615c9c0a5
Kip Tatum <kiptatum@gmail.com>
2017-10-14 00:53:56 -0700

    intitial tutorial commit

---
+++ app/src/main/java/com/example/daniel/heymayo/IntroActivity.java
@@ -0,0 +1,42 @@
+package com.example.daniel.heymayo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by kipta on 10/13/2017.
+ */
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //default slides from AppIntro
+        addSlide(SampleSlide.newInstance(R.layout.slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.slide_3));
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
---
+++ app/src/main/java/com/example/daniel/heymayo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.daniel.heymayo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kipta on 10/14/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
+
+    }
+}
