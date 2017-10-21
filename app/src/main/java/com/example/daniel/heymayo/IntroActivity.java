f60ca8d39f5b422eda3ec4b16d89afd0b0128ade
Kip Tatum <kiptatum@gmail.com>
2017-10-21 16:31:15 -0700

    update IntroActivity

--- app/src/main/java/com/example/daniel/heymayo/IntroActivity.java
+++ app/src/main/java/com/example/daniel/heymayo/IntroActivity.java
@@ -1,31 +1,49 @@
 package com.example.daniel.heymayo;
 
 import android.Manifest;
-import android.graphics.drawable.AnimationDrawable;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.widget.ImageView;
+import android.support.v4.content.ContextCompat;
+import android.location.Location;
+import android.util.Log;
+import android.widget.Toast;
+
 
 import com.github.paolorotolo.appintro.AppIntro;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by kipta on 10/13/2017.
  */
 
 
-public class IntroActivity extends AppIntro {
+public class IntroActivity extends AppIntro implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
+    private GoogleApiClient googleApiClient;
 
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+
+
         //default slides from AppIntro
         addSlide(SampleSlide.newInstance(R.layout.slide_1));
         addSlide(SampleSlide.newInstance(R.layout.slide_2));
         addSlide(SampleSlide.newInstance(R.layout.slide_3));
         addSlide(SampleSlide.newInstance(R.layout.slide_4));
+
+
 
         //you can control skip button with this. true or false method
         showSkipButton(false);
@@ -52,14 +70,62 @@ public class IntroActivity extends AppIntro {
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
-        // Do something when users tap on Done button.
-        finish();
+        // When Done is pressed app asks for location permission.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+        }
+
     }
+    //if permission is granted we switch to MapsActivity, if not toast says Need your location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // All good!
+                    Intent goToMap = new Intent(this, MapsActivity.class);
+                    startActivity(goToMap);
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
 
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
         // Do something when the slide changes.
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(MainActivity.class.getSimpleName(), "Can't connect to Google Play Services!");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 }
--- app/src/main/java/com/example/daniel/heymayo/MainActivity.java
+++ app/src/main/java/com/example/daniel/heymayo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
 /**
  * Created by kipta on 10/14/2017.
  */
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, IntroActivity.class);
         startActivity(intent);
-
     }
+
+
+
 }
