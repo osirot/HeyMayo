434e62f24459cb237bd209a3bd9e8e838bac2f77
Joseph Sayler <joseph.sayler@gmail.com>
2017-10-20 21:58:10 -0700

    updated FirebasePostActivity.java with logout menu
    added logout menu to this activity (thought it was there, but i had to reimplement it)

--- app/src/main/java/com/example/daniel/heymayo/FirebasePostActivity.java
+++ app/src/main/java/com/example/daniel/heymayo/FirebasePostActivity.java
@@ -1,13 +1,17 @@
 package com.example.daniel.heymayo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -124,4 +128,23 @@ public class FirebasePostActivity extends BaseActivity {
         mDatabase.updateChildren(childUpdates);
     }
     // [END write_fan_out]
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
