4e1c2c0da3124c9c9cc1afc9273614a436a695f1
Joseph Sayler <joseph.sayler@gmail.com>
2017-10-21 17:45:19 -0700

    added tokenID
    added a tokenID to be generated and used in posts to the database. other minor maintenance tweaks also

--- app/src/main/java/com/example/daniel/heymayo/models/User.java
+++ app/src/main/java/com/example/daniel/heymayo/models/User.java
@@ -8,14 +8,16 @@ public class User {
 
     public String username;
     public String email;
+    public String tokenID;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username, String email) {
+    public User(String username, String email, String tokenID) {
         this.username = username;
         this.email = email;
+        this.tokenID = tokenID;
     }
 
 }
