b843be8db50c3df5b86bb094eb8f5b8c3b32a67c
Kip Tatum <kiptatum@gmail.com>
2017-10-26 19:28:22 -0700

    code to run tut once

--- app/src/main/java/com/example/daniel/heymayo/MainActivity.java
+++ app/src/main/java/com/example/daniel/heymayo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.daniel.heymayo;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.audiofx.PresetReverb;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -12,13 +13,38 @@ import android.view.View;
  */
 
 public class MainActivity extends BaseActivity {
+    final String firstStart = "firstStart";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread t = new Thread(new Runnable() {
+        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //set true boolean is first time app is ran after download
+        boolean isFirstStart = mPrefs.getBoolean(firstStart, true);
+
+        //isFirstStart run tutorial
+        if (isFirstStart){
+            Intent runTutorial = new Intent (this, IntroActivity.class);
+            startActivity(runTutorial);
+
+            SharedPreferences.Editor editor = mPrefs.edit();
+            editor.putBoolean(firstStart, false);
+            editor.apply();
+        }
+
+        //not first start go to display map with location
+        if(!isFirstStart){
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+
+        }
+
+
+
+       /* Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 //  Initialize SharedPreferences
@@ -51,13 +77,18 @@ public class MainActivity extends BaseActivity {
 
             }
         });
-        t.start();
+        t.start();*/
+
+
     }
-  
-    public void mapActivity(View view) {
+
+    /*public void mapActivity(View view) {
+
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
-    }
+    }*/
+  
+
 
     public void postActivity(View view) {
         Intent intent = new Intent(this, SignInActivity.class);
